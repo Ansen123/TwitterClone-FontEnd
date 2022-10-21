fd29719dd96bd519e7a621da6b640b6a45d4d900
Ansen123 <ansenfp@gmail.com>
2022-10-21 12:45:00 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ViewuserComponent } from './viewuser/viewuser.component';
+const appRoutes:Routes=[
+  {
+path:"",component:LoginComponent
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ViewuserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private myrouter:Router) { }
+  username=""
+  password=""
+  readValues=()=>{
+    let data={
+      "username":this.username,
+      "password":this.password
+  
+    }
+    if(this.username=="admin" && this.password=="admin"){
+      this.myrouter.navigate(["/home"])
+    }
+    else{
+      alert("in valid")
+    }
+    console.log(data)
+    alert("Sucess")
+  }
+    ngOnInit(): void {
+    }
+  
+  }
